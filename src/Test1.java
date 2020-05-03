6b17a91f53aa99bdc17f9aa646eefd18610ea50d
Tejesh <ishan.tejesh12@gmail.com>
2020-05-03 18:08:05 +0530

    code changes done

--- src/Test1.java
+++ src/Test1.java
@@ -3,7 +3,8 @@ public class Test1 {
 	
 	public static void main(String[] args) {
 		boolean result=true;
-		if(result)
+		String rs = "true";
+		if(result && rs.equals("true"))
 			System.out.println("True");
 		else
 			System.out.println("False");
