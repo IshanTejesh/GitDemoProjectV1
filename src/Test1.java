690bfeecbab6642e97edbee5efba8bb80fda739f
Tejesh <ishan.tejesh12@gmail.com>
2020-05-03 16:56:10 +0530

    Commiting Test1 to BranchX

---
+++ src/Test1.java
@@ -0,0 +1,12 @@
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		boolean result=true;
+		if(result)
+			System.out.println("True");
+		else
+			System.out.println("False");
+	}
+
+}
