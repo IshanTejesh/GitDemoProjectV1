b9dfcfeb995ed1e58dae5cf61e8c38bde5ddc782
Tejesh <ishan.tejesh12@gmail.com>
2020-05-03 15:32:11 +0530

    First Commit....

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+	
+	public static void main(String[] args) {
+		boolean result=true;
+		if(result)
+			System.out.println("True");
+		else
+			System.out.println("False");
+	}
+
+}
